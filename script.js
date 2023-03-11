87414b7938ac2763d1a5a388f056b69e1e738be1
sire67 <uchepibe@gmail.com>
2023-03-11 19:07:56 +0100

    feat:added second project

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const activePage = window.location.pathname;
-console.log(activePage,"hello")
+const navlinks = document.querySelectorAll('.navlink').
+forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active')
+        console.log("hello")
+    }
+});
